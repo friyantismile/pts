42dfe8a038c80486810b6ef726efe237f2bbf385
friyantismile <friyantismile@gmail.com>
2020-08-17 12:29:26 +0800

    removed database files

--- database/database_connection.php
+++ database/database_connection.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_password = "";
-$db_name = "db_procurement";
+$db_name = "pts";
 
 $connection = mysqli_connect($db_host,$db_username,$db_password,$db_name);
 	//$database = mysqli_select_db($db_name,$connection);
